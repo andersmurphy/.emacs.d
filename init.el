f336794177eb2849e31c1ebd28d8c27e0c1e9d68
anders <andersmurphy@gmail.com>
2017-01-08 21:51:14 +0000

    Initial commit

---
+++ init.el
@@ -0,0 +1,39 @@
+;; Adds MELPA package repository. This must come before package-initialize
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+;; Added by Package.el. This must come before configurations of
+;; installed packages.
+(package-initialize)
+
+;; Sets theme
+(load-theme 'zenburn t)
+
+;; Removes menu bar
+(menu-bar-mode -1)
+
+;; Enables ido mode for fuzzy matching
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+;; Disables start up messages
+(setq inhibit-startup-message t
+inhibit-startup-echo-area-message t)  
+
+;; Enables auto indent on RET
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (zenburn-theme atom-one-dark-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
