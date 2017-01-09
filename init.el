def2102132770144bc67e05a1ec3ffd965480596
anders <andersmurphy@gmail.com>
2017-01-09 21:51:02 +0000

    Init is now organised with org-mode

--- init.el
+++ init.el
@@ -1,41 +1,39 @@
-;; Adds MELPA package repository. This must come before package-initialize
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
-
-;; Added by Package.el. This must come before configurations of
-;; installed packages.
-(package-initialize)
-
-;; Sets theme
-(load-theme 'zenburn t)
-
 ;; Removes menu scroll and tool bar
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 
-;; Ivy completion configuration
-(setq ivy-use-virtual-buffers t)
-(setq ivy-count-format "(%d/%d) ")
-(ivy-mode 1)
-
-;; Magit configuration
-(global-set-key (kbd "C-x g") 'magit-status)
-
 ;; Disables start up messages
 (setq inhibit-startup-message t
-inhibit-startup-echo-area-message t)  
+      inhibit-startup-echo-area-message t)
+(setq initial-scratch-message "")
 
-;; Enables auto indent on RET
-(define-key global-map (kbd "RET") 'newline-and-indent)
+;; Set up package and adds MELPA
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
 
+;;; Bootstrap use-package
+;; Install use-package if it's not already installed.
+;; use-package is used to configure the rest of the packages.
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+;; From use-package README
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
+
+;;; Load the config
+(org-babel-load-file (concat user-emacs-directory "config.org"))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit ivy parinfer zenburn-theme))))
+ '(package-selected-packages (quote (magit zenburn-theme use-package ivy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
