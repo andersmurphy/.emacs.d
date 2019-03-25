a1837caf93447c96528716d823264c69d4796635
andersmurphy <andersmurphy@gmail.com>
2019-03-25 14:14:10 +0000

    Auto generate setup script

---
+++ osx-setup.sh
@@ -0,0 +1,35 @@
+defaults write com.apple.finder AppleShowAllFiles -boolean true ; killall Finder
+
+xcode-select --install
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew update
+
+git config --global user.name "andersmurphy"
+git config --global user.email "andersmurphy@gmail.com"
+
+git config --global core.editor 'emacsclient'
+
+git config --global pull.rebase true
+
+git config --global color.ui true
+
+echo '.projectile
+.DS_Store' > .gitignore
+
+git config --global core.excludesfile '~/.gitignore'
+
+brew cask install emacs
+
+brew install aspell
+
+brew install mpv
+
+brew install multimarkdown
+
+brew install clojure
+
+brew install leiningen
+
+brew install joker
