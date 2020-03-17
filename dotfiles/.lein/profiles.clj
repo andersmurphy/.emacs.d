21394df16a1080cc33d260b499155f3b64018196
andersmurphy <andersmurphy@gmail.com>
2020-03-17 22:35:53 +0000

    Add git dependency for repl enrichments

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -1,3 +1,8 @@
 {:user {:dependencies [[compliment "0.3.8"]
                        [org.clojure/tools.namespace "0.3.0"]
-                       [pjstadig/humane-test-output "0.9.0"]]}}
+                       [pjstadig/humane-test-output "0.9.0"]
+                       [clj-repl-enrichment "58e1548f60683475649e82951ae698d11ea415ed"]]
+        :plugins      [[reifyhealth/lein-git-down "0.3.6"]]
+        :middleware   [lein-git-down.plugin/inject-properties]
+        :repositories [["public-github" {:url "git://github.com/"}]]
+        :git-down     {clj-repl-enrichment {:coordinates andersmurphy/clj-repl-enrichment}}}}
