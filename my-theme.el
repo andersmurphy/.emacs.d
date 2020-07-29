68f248e8a53edacda00f252cf4fc10067da01684
andersmurphy <andersmurphy@gmail.com>
2020-07-29 12:31:12 +0100

    Start moving universal themes into my-theme

--- my-theme.el
+++ my-theme.el
@@ -1,8 +1,15 @@
 (deftheme my
   "Created 2020-07-20.")
 
-(custom-theme-set-faces
- 'my
- '(default ((t (:family "Menlo" :foundry "nil" :width normal :height 140 :weight normal :slant normal :underline nil :overline nil :extend nil :strike-through nil :box nil :inverse-video nil :foreground "#222222" :background "#f2f2f2" :stipple nil :inherit nil)))))
+(let ((bg "#f2f2f2")
+      (fg "#222222"))
+  (custom-theme-set-faces
+   'my
+   `(default                     ((t (:foreground ,fg :background ,bg))))
+   `(cursor                      ((t (:foreground ,fg :weight bold))))
+   `(minibuffer-prompt           ((t (:foreground ,fg))))
+   `(fringe                      ((t (:background nil))))
+   `(emms-playlist-track-face    ((t (:foreground ,fg :background ,bg))))
+   `(emms-playlist-selected-face ((t (:foreground ,fg :background ,bg :weight bold))))))
 
 (provide-theme 'my)
