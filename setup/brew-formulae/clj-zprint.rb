c0b0c88e5b5a08ec07e94b905e577c206caaac09
andersmurphy <andersmurphy@gmail.com>
2020-08-17 14:55:45 +0100

    Simplify zprint brew script

--- setup/brew-formulae/clj-zprint.rb
+++ setup/brew-formulae/clj-zprint.rb
@@ -14,14 +14,17 @@ class CljZprint < Formula
   def install
     system "mv zprintm-1.0.0 clj-zprint"
     system "chmod 755 clj-zprint"
-    system "echo '#!/bin/bash
-\"clj-zprint\" \"$1\" < \"$2\"' > wrap-clj-zprint"
+    open('wrap-clj-zprint', 'w') { |f|
+      f.write "#!/bin/bash
+ clj-zprint \"$1\" < \"$2\""
+    }
     system "chmod 755 wrap-clj-zprint"
     bin.install "clj-zprint"
     bin.install "wrap-clj-zprint"
   end
 
   test do
-    system "false"
+    system "#{bin}/clj-zprint"
+    system "#{bin}/wrap-clj-zprint"
   end
 end
