a4e70f161e28e1c926d44c4c4207b126ce2be81b
andersmurphy <andersmurphy@gmail.com>
2021-01-16 14:45:26 +0000

    Remove apheleia: currently prefer formatting in pre-commit hooks

--- init.el
+++ init.el
@@ -761,25 +761,10 @@
 
 ;;; Programming
 (progn ;; Defaults
-
   ;; Show paren mode
   (show-paren-mode 1)
   (defvar show-paren-delay)
   (setq show-paren-delay 0))
-;; Formatting
-(use-package apheleia
-  :straight (apheleia :host github :repo "raxod502/apheleia")
-  :config
-  (setf (alist-get 'prettier apheleia-formatters)
-        '(npx "prettier"
-              "--trailing-comma"  "es5"
-              "--bracket-spacing" "true"
-              "--single-quote"    "true"
-              "--semi"            "false"
-              "--print-width"     "100"
-              file))
-  (add-to-list 'apheleia-mode-alist '(rjsx-mode . prettier))
-  (apheleia-global-mode t))
 ;; Lisp
 (use-package inf-lisp
   :bind (:map inferior-lisp-mode-map
