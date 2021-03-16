5712dd9a7c3245462f0fbde7833db74435b1a9bd
Anders Murphy <andersmurphy@gmail.com>
2021-03-16 19:41:47 +0000

    Update osx-setup.sh

--- setup/scripts/osx-setup.sh
+++ setup/scripts/osx-setup.sh
@@ -4,7 +4,7 @@ defaults write com.apple.dock autohide -bool true
 
 xcode-select --install
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew update
 
