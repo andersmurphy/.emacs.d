c20713183fdc5f11afcedcda4fad50cd673f9e30
andersmurphy <andersmurphy@gmail.com>
2021-04-18 15:33:40 +0100

    Add wait commands to setup script

--- setup/scripts/osx-setup.sh
+++ setup/scripts/osx-setup.sh
@@ -3,21 +3,28 @@ defaults write -g ApplePressAndHoldEnabled -bool true
 defaults write com.apple.dock autohide -bool true
 
 xcode-select --install
+wait
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+wait
 
 brew update
+wait
 
 brew analytics off
 brew tap homebrew/cask
 
 brew install --cask chrysalis
+wait
 
 brew install svn
+wait
 brew tap homebrew/cask-fonts
 brew install font-dm-mono
+wait
 
 brew install git
+wait
 
 git config --global user.name "andersmurphy"
 git config --global user.email "andersmurphy@gmail.com"
@@ -39,29 +46,38 @@ git config --global diff.algorithm histogram
 
 brew tap daviderestivo/emacs-head
 brew install emacs-head@28 --with-cocoa --with-imagemagick --with-xwidgets
+wait
 
 brew install aspell
+wait
 
 brew install mpv
+wait
 rm ~/.config/mpv/mpv.conf
 ln -s ~/.emacs.d/setup/dotfiles/.mpv/mpv.conf ~/.config/mpv/
 
 brew install multimarkdown
+wait
 
 brew tap AdoptOpenJDK/openjdk
 brew install adoptopenjdk11
+wait
 
 brew install clojure
+wait
 rm ~/.clojure/deps.edn
 ln -s ~/.emacs.d/setup/dotfiles/.clojure/deps.edn ~/.clojure/
 
 brew install leiningen
+wait
 rm ~/.lein/profiles.clj
 ln -s ~/.emacs.d/setup/dotfiles/.lein/profiles.clj ~/.lein/
 
 brew install --build-from-source ~/.emacs.d/setup/brew-formulae/clj-zprint.rb
+wait
 
 brew install clojure-lsp/brew/clojure-lsp-native
+wait
 rm -r ~/.lsp
 mkdir ~/.lsp
 ln -s ~/.emacs.d/setup/dotfiles/.lsp/config.edn ~/.lsp/
@@ -71,14 +87,19 @@ mkdir ~/.clj-kondo
 ln -s ~/.emacs.d/setup/dotfiles/.clj-kondo/config.edn ~/.clj-kondo/
 
 brew install node
+wait
 
 npm install -g yarn
+wait
 
 npm i -g typescript-language-server; npm i -g typescript
 
 npm install -g vscode-html-languageserver-bin
+wait
 
 brew install chromium
+wait
 open -a "Chromium" --args --make-default-browser
 
 brew install --cask dropbox
+wait
