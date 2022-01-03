3cffb94a455afee471fddc7719357cfb2cee4b71
andersmurphy <andersmurphy@gmail.com>
2022-01-03 22:32:35 +0000

    Add key frequency analytics

--- init.el
+++ init.el
@@ -1010,6 +1010,14 @@ If this becomes a problem these common lines could be filtered."
          (eww-mode . variable-pitch-mode)
          (eww-after-render . eww-readable)))
 
+;;; ANALYTICS
+(use-package keyfreq
+  :config
+  (keyfreq-mode 1)
+  (keyfreq-autosave-mode 1)
+  (setq keyfreq-excluded-commands
+        '(self-insert-command)))
+
 ;;; LOAD PROJECT SPECIFIC COMMANDS
 (when (file-directory-p "~/.emacs.d/emacs-sync")
   (load "~/.emacs.d/emacs-sync/project-specific-commands.el"))
