a91c285a30ecc702650a949d1d3cb408b91e7690
andersmurphy <andersmurphy@gmail.com>
2022-04-09 18:51:50 +0100

    update create clojure project function to 1.11.1

--- modes/clj.el
+++ modes/clj.el
@@ -398,7 +398,7 @@ Works from both namespace and test namespace"
     (make-directory project-name-path)
     (find-file (concat project-name-path "/deps.edn"))
     (insert "{:paths [\"src\"]
- :deps {org.clojure/clojure {:mvn/version \"1.11.0\"}}
+ :deps {org.clojure/clojure {:mvn/version \"1.11.1\"}}
  :aliases {}}")
     (save-buffer)
     (make-directory (concat project-name-path "/src"))
