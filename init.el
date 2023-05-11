15e016250472b6fd0ee2be3caf1ee2e27ed53476
andersmurphy <andersmurphy@gmail.com>
2023-05-11 21:47:24 +0100

    remove unused use-package init

--- init.el
+++ init.el
@@ -881,7 +881,6 @@ If this becomes a problem these common lines could be filtered."
 (load "~/.emacs.d/modes/topiary.el")
 (use-package topiary
   :straight nil
-  :init
   :hook ((text-mode prog-mode comint-mode outline-mode Info-mode eshell-mode magit-blob-mode) . topiary-mode))
 (use-package special-mode
   :straight nil
