144737a1df434a1dbd9ee0017b20c14aabded038
andersmurphy <andersmurphy@gmail.com>
2023-12-06 15:57:50 +0000

    Add comment about emms breakages

--- init.el
+++ init.el
@@ -1244,6 +1244,24 @@ If this becomes a problem these common lines could be filtered."
   (setq nov-text-width 80)
   :hook (nov-mode . my/nov-font-setup))
 (use-package emms
+  ;; NOTE: emms tends to break now and then with byte compilation.
+  ;; you can test different version by using the commit hash for
+  ;; the commit you care about. Can be found here:
+  ;; 
+  ;; https://github.com/emacsmirror/emms/commits/master/
+  ;;
+  ;; You'll then need to change the emms value in
+  ;;
+  ;; .emacs.d/straight/versions/default.el
+  ;;
+  ;; And delete:
+  ;; 
+  ;; .emacs.d/straight/repos/emms.el
+  ;;
+  ;; Finally restart emacs to re-download the repo.
+  ;;
+  ;; Sometimes there can also be issues with the byte compilation caching
+  ;; old files.
   :config
   (emms-all)
   (emms-default-players)
