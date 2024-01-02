66d3cf23c18aaa3b64b115773e03936f32fa5d39
andersmurphy <andersmurphy@gmail.com>
2024-01-02 12:43:32 +0000

    Update clojure global deps

--- setup/dotfiles/.lein/profiles.clj
+++ setup/dotfiles/.lein/profiles.clj
@@ -1,2 +1,2 @@
-{:user {:dependencies [[org.clojure/tools.namespace "0.3.0"]
-                       [pjstadig/humane-test-output "0.9.0"]]}}
+{:user {:dependencies [[org.clojure/tools.namespace "1.4.5"]
+                       [pjstadig/humane-test-output "0.11.0"]]}}
