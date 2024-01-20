f0541d6b2cf58eb334be09416fdd24dd8b6f01e5
andersmurphy <andersmurphy@gmail.com>
2024-01-20 20:53:37 +0000

    Remove unused lein user dependency

--- setup/dotfiles/.lein/profiles.clj
+++ setup/dotfiles/.lein/profiles.clj
@@ -1,2 +1,1 @@
-{:user {:dependencies [[org.clojure/tools.namespace "1.4.5"]
-                       [pjstadig/humane-test-output "0.11.0"]]}}
+{:user {:dependencies [[org.clojure/tools.namespace "1.4.5"]]}}
