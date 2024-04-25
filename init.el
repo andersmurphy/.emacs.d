b493fc181d4dadc19c3f7633fea24ba08be81fff
andersmurphy <andersmurphy@gmail.com>
2024-04-25 20:28:26 +0100

    Found out project-eshell does exactly what I need

--- init.el
+++ init.el
@@ -328,23 +328,6 @@ This can be used to make the window layout change based on frame size."
   ;; Convenience mode for killing buffer on q
   :straight nil
   :load-path "~/.emacs.d/elisp")
-(use-package eshell
-  :straight nil
-  :init
-  (defun my/eshell ()
-    "Open eshell in project root, if open switch to existing eshell."
-    (interactive)
-    (if-let ((root-dir (or (vc-root-dir) (magit-toplevel))))
-        (let* ((project-name (file-name-nondirectory
-                              (directory-file-name root-dir)))
-               (eshell-buffer-name (concat "*eshell*<" project-name ">")))
-          (eshell))
-      (eshell)))
-
-  (defun my/eshell-new ()
-    "Open a new instance of eshell."
-    (interactive)
-    (eshell 'N)))
 
 ;;; PRIVACY
 (progn ;; GPG
