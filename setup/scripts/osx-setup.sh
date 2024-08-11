a79e56ffe12af7b5196a62871aa7018da31b04bd
andersmurphy <andersmurphy@gmail.com>
2024-08-11 21:06:20 +0100

    Update solana setup

--- setup/scripts/osx-setup.sh
+++ setup/scripts/osx-setup.sh
@@ -131,5 +131,7 @@ wait
 npm install nbb -g
 
 ### solana ###
-brew install solana
+# homebrew version doesn't come with a test validator
+sh -c "$(curl -sSfL https://release.anza.xyz/stable/install)"
+echo 'export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"' >> ~/.zshrc
 wait
