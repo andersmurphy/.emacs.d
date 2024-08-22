0de46aaadf93821e419f12010a3af4567ca03450
andersmurphy <andersmurphy@gmail.com>
2024-08-22 10:20:57 +0100

    Add humane test ouput to lein config

--- setup/dotfiles/.lein/profiles.clj
+++ setup/dotfiles/.lein/profiles.clj
@@ -1,1 +1,2 @@
-{:user {:dependencies [[org.clojure/tools.namespace "1.4.5"]]}}
+{:user {:dependencies [[org.clojure/tools.namespace "1.4.5"]
+                       [pjstadig/humane-test-output "0.11.0"]]}}
