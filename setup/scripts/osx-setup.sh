cfc2022b67098f8a0882f8439d9016a041c156cc
Anders Murphy <andersmurphy@gmail.com>
2024-12-30 10:05:16 +0000

    Fix typo in setup script

--- setup/scripts/osx-setup.sh
+++ setup/scripts/osx-setup.sh
@@ -25,7 +25,7 @@ ln -s ~/.emacs.d/setup/dotfiles/.gitignore ~/.gitignore
 git config --global core.excludesfile '~/.gitignore'
 
 ### EMACS (build from source) ###
-brew install libxml2 gcc libgccjit tree-sitter jansson pkg-config gnutls textinfo autoconf
+brew install libxml2 gcc libgccjit tree-sitter jansson pkg-config gnutls texinfo autoconf
 wait
 # We clone just the tag for a smaller download
 git clone --depth 1 --branch emacs-29.3 https://git.savannah.gnu.org/git/emacs.git
