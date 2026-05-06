e57630eef70f8ac74f2368cba8f329580ba2bc20
andersmurphy <andersmurphy@gmail.com>
2026-05-06 15:31:06 +0100

    Add support for slides

--- init.el
+++ init.el
@@ -1232,6 +1232,44 @@ files in the project. Respects gitignore."
                   (get-char-property (point) 'face))))
     (if face (message "Face: %s" face) (message "No face at %d" pos))))
 
+;;; Slides
+(use-package visual-fill-column
+  :ensure t
+  :config)
+(use-package org-present
+  :ensure t
+  :config
+  (setq visual-fill-column-width 110
+        visual-fill-column-center-text t)
+  (defun my/org-present-start ()
+    (setq header-line-format " ")
+    (topiary-mode -1)
+    (visual-fill-column-mode 1)
+    (setq-local face-remapping-alist
+                '((default (:height 1.5) variable-pitch)
+                  (header-line (:height 4.0) variable-pitch)
+                  (org-document-title (:height 1.75) org-document-title)
+                  (org-code (:height 1.55) org-code)
+                  (org-verbatim (:height 1.55) org-verbatim)
+                  (org-block (:height 1.25) org-block)
+                  (org-block-begin-line (:height 0.7) org-block)))
+    (variable-pitch-mode 1)
+    (org-display-inline-images)
+    (org-present-hide-cursor)
+    (org-present-read-only))
+  (defun my/org-present-end ()
+    (setq header-line-format nil)
+    (topiary-mode 1)
+    (visual-fill-column-mode 0)
+    (setq-local face-remapping-alist '((default variable-pitch default)))
+    (variable-pitch-mode 0)
+    (org-remove-inline-images)
+    (org-present-show-cursor)
+    (org-present-read-write))
+  :hook
+  ((org-present-mode . my/org-present-start)
+   (org-present-mode-quit . my/org-present-end)))
+
 
 ;;; MEDIA
 (use-package image-mode
